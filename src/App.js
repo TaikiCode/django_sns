84e8923958e18262adc1d4d49c7f8c3d1a3a2190
TaikiCode <metoro5151@gmail.com>
2021-02-05 01:08:12 +0900

    create contact section

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import Portfolio from "./components/Portfolio";
 import Experience from "./components/Experience";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -36,6 +37,7 @@ function App() {
       <AboutMe />
       <Experience />
       <Portfolio />
+      <Contact />
     </>
   );
 }
